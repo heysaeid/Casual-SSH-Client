136958c34008f767031c66ce2d1e7ff864ab682e
heysaeid <heysaeid92@gmail.com>
2023-03-15 23:45:57 +0330

    Add ability to add ssh session

--- SSHClient.sh
+++ SSHClient.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+SSH_CONFIG_FILE="$HOME/.ssh/sessions.conf"
+
+if [[ "$1" == "--add" ]]; then
+  # Prompt the user for the new SSH session information
+  read -p "Enter a name for the new SSH session: " SSH_NAME
+  read -p "Enter the remote host for the new SSH session: " SSH_HOST
+  read -p "Enter the SSH user for the new SSH session: " SSH_USER
+  read -p "Enter the SSH port for the new SSH session (leave blank for default): " SSH_PORT
+
+  # If no SSH port was provided, use the default (22)
+  if [[ -z "$SSH_PORT" ]]; then
+    SSH_PORT=22
+  fi
+
+  # Append the new SSH session to the configuration file
+  echo "${SSH_NAME},${SSH_HOST},${SSH_USER},${SSH_PORT}" >> "$SSH_CONFIG_FILE"
+  echo "SSH session added: $SSH_NAME"
+  exit 0
+fi
+
 # Check if the user specified a number of panes
 if [[ $# -eq 0 ]]; then
   echo "Usage: $0 <num_panes>"
